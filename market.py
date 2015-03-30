eaf3fd0e480abd9c35e2e85af7c704a9ce290fdb
jcpinyan <jcpinyan@users.noreply.github.com>
2015-03-30 14:17:44 -0400

    added name to Ingredient

--- market.py
+++ market.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
 class Ingredient:
-   def __init__(self, availability, color, shape):
-      self.availability = availability
+   def __init__(self, name, canBuy, color, shape):
+      self.name = name
+      self.canBuy = canBuy
       self.color = color
       self.shape = shape
 
    def __str__(self):
-      return 'Ingredient (%s, %s, %s)' % (self.availability, self.color, self.shape)
+      return 'Ingredient (%s, %s, %s, %s)' % (self.name, self.canBuy, self.color, self.shape)
