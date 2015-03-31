3fb06e3c99cd5707e0d1e19eff232910728e6789
jcpinyan <jcpinyan@users.noreply.github.com>
2015-03-31 15:43:03 -0400

    New Supply class for baskets and market

--- market.py
+++ market.py
@@ -24,12 +24,36 @@ herbs  = Ingredient('herbs',  False, 'brown',  'hourglass')
 players = 4
 maxIngred = players+2
 
+class Supply:
+   '''Ingredients available in the market'''
+   def __init__(self, name, carrots, onions, celeries, peppers, garlics, bouquets):
+      self.name = name
+      self.carrots = carrots
+      self.onions = onions
+      self.celeries = celeries
+      self.peppers = peppers
+      self.garlics = garlics
+      self.bouquets = bouquets
+
+   def __hash__(self):
+      return hash(self.name)
+
+   def __str__(self):
+      return 'Supply("%s", %d, %d, %d, %d, %d, %d)' % (self.name, self.carrots, self.onions, self.celeries, self.peppers, self.garlics, self.bouquets)
+
+   def display(self):
+      '''displays the contents of the basket formatted nicely'''
+      return '%s\nCarrots: %d\nOnions: %d\nCeleries: %d\nPeppers: %d\nGarlics: %d\nHerb Bouquets: %d' % (self.name, self.carrots, self.onions, self.celeries, self.peppers, self.garlics, self.bouquets)
+
+
 # initialize market
-farmersMarket = {i:maxIngred for i in [carrot,onion,celery,pepper,garlic]}
-farmersMarket[herbs]=5
+#farmersMarket = {i:maxIngred for i in [carrot,onion,celery,pepper,garlic]}
+#farmersMarket[herbs]=5
+farmersMarket = Supply('farmersMarket',maxIngred,maxIngred,maxIngred,maxIngred,maxIngred,5)
 
 # initialize basket
-basket = {i:0 for i in farmersMarket}
+# basket = {i:0 for i in farmersMarket}
+basket = Supply('basket',0,0,0,0,0,0)
 
 def purchase(basket,farmersMarket,shoppingDict):
    '''moves requested items from farmers market to basket'''
