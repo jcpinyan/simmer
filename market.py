a3125990673906029c5e1e53616129d3fde41f68
Jennifer VB <jcpinyan@users.noreply.github.com>
2015-04-13 20:25:22 -0400

    fixed tabs and line breaks

--- market.py
+++ market.py
@@ -3,18 +3,18 @@
 from collections import defaultdict
 
 class Ingredient:
-   '''Ingredients available in the market'''
-   def __init__(self, name, canBuy, color, shape):
-      self.name = name
-      self.canBuy = canBuy
-      self.color = color
-      self.shape = shape
+    '''Ingredients available in the market'''
+    def __init__(self, name, canBuy, color, shape):
+        self.name = name
+        self.canBuy = canBuy
+        self.color = color
+        self.shape = shape
 
-   def __hash__(self):
-      return hash(self.name)
+    def __hash__(self):
+        return hash(self.name)
 
-   def __str__(self):
-      return 'Ingredient("%s", %s, "%s", "%s")' % (self.name, self.canBuy, self.color, self.shape)
+    def __str__(self):
+        return 'Ingredient("{0}", {1}, "{2}", "{3}")'.format(self.name, self.canBuy, self.color, self.shape)
 
 # global definitions of ingredients
 carrot = Ingredient('carrot', True,  'orange', 'triangle')
@@ -31,27 +31,33 @@ HERBS = 5
 from collections import OrderedDict
 
 class Supply:
-   '''Ingredients available in the market'''
-   def __init__(self, name, carrots, onions, celeries, peppers, garlics, bouquets):
-      try:
-         assert isinstance(carrots,int) and isinstance(onions,int) and isinstance(celeries,int) and isinstance(peppers,int) and isinstance(garlics,int) and isinstance(bouquets,int)
-      except AssertionError:
-         print('ingredients must be ints')
-         raise
-      self.name = name
-      self.ingredients = OrderedDict([(carrot,carrots), (onion,onions), (celery,celeries), (pepper,peppers), (garlic,garlics), (herbs,bouquets)])
+    '''Ingredients available in the market'''
+    def __init__(self, name, carrots, onions, celeries, peppers, garlics, bouquets):
+        try:
+            assert isinstance(carrots,int) and \
+                    isinstance(onions,int) and \
+                    isinstance(celeries,int) and \
+                    isinstance(peppers,int) and \
+                    isinstance(garlics,int) and \
+                    isinstance(bouquets,int)
+        except AssertionError:
+            print('ingredients must be ints')
+            raise
+        self.name = name
+        self.ingredients = OrderedDict([(carrot,carrots), (onion,onions), (celery,celeries), (pepper,peppers), (garlic,garlics), (herbs,bouquets)])
 
-   def __hash__(self):
-      return hash(self.name)
+    def __hash__(self):
+        return hash(self.name)
 
-   def __str__(self):
-      return 'Supply("%s", %d, %d, %d, %d, %d, %d)' % (self.name, self.ingredients[carrot], self.ingredients[onion], self.ingredients[celery], self.ingredients[pepper], self.ingredients[garlic], self.ingredients[herbs])
+    def __str__(self):
+        return 'Supply("%s", %d, %d, %d, %d, %d, %d)' % (self.name, self.ingredients[carrot], self.ingredients[onion], self.ingredients[celery], self.ingredients[pepper], self.ingredients[garlic], self.ingredients[herbs])
 
-   def publish(self):
-      '''prints name of Supply and how many of each item'''
-      print(self.name)
-      for (k,v) in self.ingredients.items():
-         print(k.name,v,sep='\t')
+    def publish(self):
+        '''prints name of Supply and how many of each item'''
+        print(self.name)
+        for (k,v) in self.ingredients.items():
+            print(k.name,v,sep='\t')
+
 
 
 # initialize market
@@ -61,46 +67,50 @@ farmersMarket = Supply('farmersMarket',maxIngred,maxIngred,maxIngred,maxIngred,m
 basket = Supply('basket',0,0,0,0,0,0)
 
 def checkMarket(farmersMarket,request):
-   '''verify that market has the requested amounts'''
-   for (k,v) in request.ingredients.items():
-      try:
-         assert v <= farmersMarket.ingredients[k]
-      except AssertionError:
-         print("Farmers Market only has",farmersMarket.ingredients[k],k,"but you want",v)
-         return(False)
-   return(True)
+    '''verify that market has the requested amounts'''
+    for (k,v) in request.ingredients.items():
+        try:
+            assert v <= farmersMarket.ingredients[k]
+        except AssertionError:
+            print("Farmers Market only has",farmersMarket.ingredients[k],k,"but you want",v)
+            return(False)
+    return(True)
    
 def checkRequest(request):
-   '''verify that request is valid'''
-   inv_map = defaultdict(list) 
-   for (k, v) in request.ingredients.items():
-      inv_map[v].append(k)
-   if len(inv_map[1]) == 3 and len(inv_map[0]) == 3 and request.ingredients[herbs] == 0:
-      return(True)
-   elif len(inv_map[2]) == 1 and len(inv_map[0]) == 5 and request.ingredients[herbs] == 0:
-      return(True)
-   elif request.ingredients[herbs] == 1 and len(inv_map[0]) == 5:
-      return(True)
-   else:
-      print('invalid request')
-      return(False)
+    '''verify that request is valid'''
+    inv_map = defaultdict(list) 
+    for (k, v) in request.ingredients.items():
+        inv_map[v].append(k)
+    if len(inv_map[1]) == 3 and \
+        len(inv_map[0]) == 3 and \
+        request.ingredients[herbs] == 0:
+        return(True)
+    elif len(inv_map[2]) == 1 and \
+        len(inv_map[0]) == 5 and \
+        request.ingredients[herbs] == 0:
+        return(True)
+    elif request.ingredients[herbs] == 1 and len(inv_map[0]) == 5:
+        return(True)
+    else:
+        print('invalid request')
+        return(False)
 # TODO: error message so that it says specifically what is wrong with the request.
 
 
 def transaction(basket,farmersMarket,request):
-   '''moves Ingredients between two Supply objects
-      example: get Ingredients from market for basket
-      example: pay Ingredients from basket to market
-   '''
-   try:
-      assert checkMarket(farmersMarket, request) and checkRequest(request)
-   except AssertionError:
-      print("Try again with a valid request for this market.")
-      return(basket,farmersMarket)
-   for (k,v) in request.ingredients.items():
-      basket.ingredients[k]+=v
-      farmersMarket.ingredients[k]-=v
-   return(basket,farmersMarket)
+    '''moves Ingredients between two Supply objects
+        example: get Ingredients from market for basket
+        example: pay Ingredients from basket to market
+    '''
+    try:
+        assert checkMarket(farmersMarket, request) and checkRequest(request)
+    except AssertionError:
+        print("Try again with a valid request for this market.")
+        return(basket,farmersMarket)
+    for (k,v) in request.ingredients.items():
+        basket.ingredients[k]+=v
+        farmersMarket.ingredients[k]-=v
+    return(basket,farmersMarket)
 
 
 
