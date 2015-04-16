8f8a68ccefbd253d589c04b988078eab8ad01a69
Jennifer VB <jcpinyan@users.noreply.github.com>
2015-04-16 18:52:22 -0400

    changed ordering to be a function

--- interaction.py
+++ interaction.py
@@ -2,10 +2,9 @@
 
 from market import *
 
-order = Supply('order',0,0,0,0,0,0)
-
-for i in order.ingredients.keys():
-   order.ingredients[i] =  input("How many " + i.name + "? ")
-
-order.publish()
+def makeOrder():
+    order = Supply('order',0,0,0,0,0,0)
+    for i in order.ingredients.keys():
+        order.ingredients[i] =  input("How many " + i.name + "? ")
+    return(order)
 
