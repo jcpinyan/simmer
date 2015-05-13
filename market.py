2d827b4823f250e60e269419af7ae166590ec55b
Jennifer VB <jcpinyan@users.noreply.github.com>
2015-05-12 20:25:11 -0400

    moved ingredients into own module

--- market.py
+++ market.py
@@ -1,35 +1,11 @@
 #!/usr/bin/python
 
-from collections import defaultdict
-
-class Ingredient:
-    '''Ingredients available in the market'''
-    def __init__(self, name, canBuy, color, shape):
-        self.name = name
-        self.canBuy = canBuy
-        self.color = color
-        self.shape = shape
-
-    def __hash__(self):
-        return hash(self.name)
-
-    def __str__(self):
-        return 'Ingredient("{0}", {1}, "{2}", "{3}")'.format(self.name, \
-            self.canBuy, self.color, self.shape)
-
-# global definitions of ingredients
-carrot = Ingredient('carrot', True,  'orange', 'triangle')
-onion  = Ingredient('onion',  True,  'white',  'circle')
-celery = Ingredient('celery', True,  'yellow', 'rectangle')
-pepper = Ingredient('pepper', True,  'green',  'square')
-garlic = Ingredient('garlic', True,  'tan',    'heart')
-herbs  = Ingredient('herbs',  False, 'brown',  'hourglass')
+from collections import defaultdict, OrderedDict
+from ingredients import *
 
 players = 4
 maxIngred = players+2
 HERBS = 5
-
-from collections import OrderedDict
 
 class Supply:
     '''Ingredients available in the market'''
